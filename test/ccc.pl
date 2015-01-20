4392a133eceb547af44aa573b6d6d6bc00e3a14f
derori <derori.derori@gmail.com>
2015-01-20 15:04:28 +0900

    ディスクなくなってきたからとりあえずエンコするの。

---
+++ test/ccc.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+# その日付のファイルをエンコするよ。
+# param yyyymmdd
+use 5.20.1;
+use Data::Dumper;
+$Data::Dumper::Terse = 1;# $VAR1などをつけない
+use Path::Class;
+
+
+my $date = $ARGV[0];
+$date =~ s/^20//;
+my $delete_flg = $ARGV[1] eq "--delete" 1 : 0;
+
+
+my $C = {
+    "dir" => {"from" => "/smb/ts",
+              "to" => "/smb/ts2m4v"
+          },
+    "cli" => '/usr/local/bin/HandBrakeCLI -i "${INPUT}" -o "${OUTPUT}" --preset="High Profile"',
+    "file_extension" => "mp4"
+};
+
+my $dir = dir($C->{dir}->{from});
+while(my $file = $dir->next) {
+    next if $file eq $dir or $file->resolve eq $dir->parent; # カレントと１つ上のディレクトリを除外
+
+    if($file =~ /\[$date-/){
+        my $to = $file->basename;
+        $to =~ s/m2ts$/mp4/;
+
+        my $c = $C->{cli};
+        $c =~ s/\$\{INPUT\}/$file/;
+        $c =~ s/\$\{OUTPUT\}/$C->{dir}->{to}\/$to/;
+        warn $c;
+        `$c`;
+        if($delete_flg) unlink $file;
+
+    }
+
+
+}
